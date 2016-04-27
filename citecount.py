e4a5c7847412b89642f55e3c5e0da0c3ffc9ccfb
dkoes <dkoes@pitt.edu>
2016-04-27 11:03:59 -0400

    citecnts

---
+++ citecount.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+
+import seaborn 
+import numpy as np
+import sys,re,glob,collections,os
+import bibtexparser
+import scholar
+
+cites = set()
+
+for tex in glob.glob('*.tex'):
+    for line in open(tex):
+        m = re.search(r'^.*&.*&(.*)&(.*)& \\cite\{(.*)\}.*\\\\', line)
+        if m and m.group(1).strip() != 'License':
+            cites.add(m.group(3))
+
+seen = set()
+dois = []
+with open('bibliography/biblio.bib') as bibtex_file:
+    bib_database = bibtexparser.load(bibtex_file)
+    for e in bib_database.entries:
+        if 'ID' in e and e['ID'] in cites and 'doi' in e:
+            seen.add(e['ID'])
+            dois.append(e['doi'])
+
+notseen = cites - seen
+
+querier = scholar.ScholarQuerier()
+
+citecnts = []
+
+for doi in dois:
+    query = scholar.SearchScholarQuery()
+    query.set_words(doi)
+    querier.send_query(query)
+    
+    if len(querier.articles) > 0 :
+        art = querier.articles[0]
+        txt = art.as_txt()
+        if doi in txt:
+            #print art['num_citations'],doi
+            citecnts.append(art['num_citations'])
+        else:
+            print "NOTFOUND",doi
+    else:
+        print "REALLYNOTFOUND",doi
+        
+np.savetxt('citecnts.txt',citecnts)
+    
